dd2731b94c99d8f8584b01e75d69e0b329801b2e
Guanshan Liu <guanshan.liu@gmail.com>
2016-04-20 21:22:42 +0800

    removed duplicated code

--- RxSwiftPlayground/RxSwift.playground/Pages/Reactive.xcplaygroundpage/Contents.swift
+++ RxSwiftPlayground/RxSwift.playground/Pages/Reactive.xcplaygroundpage/Contents.swift
@@ -35,24 +35,6 @@ struct Fetcher {
     }
 }
 
-enum ParseError: ErrorType {
-    case InvalidFormat
-}
-
-func parseForMovieTitles(json: AnyObject) -> [String]? {
-    guard let dictionary = json as? [String: AnyObject],
-        let results = dictionary["results"] as? [[String: AnyObject]]
-        else {
-            return nil
-    }
-    return results.flatMap { movie -> String? in
-        if let title = movie["title"] as? String {
-            return title
-        }
-        return nil
-    }
-}
-
 class ViewController: UITableViewController {
     var results = [String]()
     
